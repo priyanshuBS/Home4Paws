ff5d8f10589f612b61c7a716e527dffc05256f33
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-07-08 19:02:17 +0530

    update utils

--- server/src/utils/ApiResponse.js
+++ server/src/utils/ApiResponse.js
@@ -1,8 +1,9 @@
 class ApiResponse {
-  constructor(data, message = "Success") {
-    this.success = true;
+  constructor(statusCode, data, message = "Success") {
+    this.statusCode = statusCode;
     this.data = data;
     this.message = message;
+    this.success = statusCode < 400;
   }
 }
 
