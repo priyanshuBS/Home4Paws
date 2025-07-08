520fc04d029d1cbad1766ae3f3c322146b14ec22
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-07-08 21:34:39 +0530

    add globle middleware and import user routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,23 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
+
+// import routes
+import userRouter from "./routes/user.route.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
 
 export default app;
