ff89d049f03e28efce3b206626d2da14aa85a9b2
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-07-09 17:24:44 +0530

    update header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
 
       {/* Mobile Sidebar */}
       <div
-        className={`fixed top-0 right-0 h-full w-64 bg-pink-50 shadow-lg transform ${
+        className={`fixed top-0 right-0 h-full w-64 bg-pink-100 shadow-lg transform ${
           isOpen ? "translate-x-0" : "translate-x-full"
         } transition-transform duration-300 ease-in-out z-50`}
       >
