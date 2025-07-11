555915a383c97c6da119df2bfe1b4b9790a51b73
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-07-11 11:09:23 +0530

    add role in auth schema

--- server/src/validations/user.validation.js
+++ server/src/validations/user.validation.js
@@ -2,14 +2,13 @@ import { z } from "zod";
 import { email } from "zod/v4";
 
 export const signupUserSchema = z.object({
+  name: z.string().trim().min(3).max(50),
   email: z.string().email("Invalid email"),
-  phoneNumber: z
-    .string()
-    .regex(/^[0-9]{10}$/, "Phone number must be exactly 10 digits"),
   password: z
     .string()
     .min(6, "Password must be at least 6 characters long")
     .max(50, "Password must not exceed 50 characters"),
+  role: z.enum(["customer", "owner", "delivery", "admin"]),
 });
 
 export const loginUserSchema = z.object({
@@ -18,4 +17,5 @@ export const loginUserSchema = z.object({
     .string()
     .min(6, "Password must be at least 6 characters long")
     .max(50, "Password must not exceed 50 characters"),
+  role: z.enum(["customer", "owner", "delivery", "admin"]),
 });
