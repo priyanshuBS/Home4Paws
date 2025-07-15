396d97d44b48000b141e4fe3c7189b48eea51ccd
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-07-16 00:09:06 +0530

    add profile validation

--- server/src/validations/user.validation.js
+++ server/src/validations/user.validation.js
@@ -19,3 +19,20 @@ export const loginUserSchema = z.object({
     .max(50, "Password must not exceed 50 characters"),
   role: z.enum(["customer", "owner", "delivery", "admin"]),
 });
+
+export const updateUserProfileSchema = z
+  .object({
+    name: z.string().min(3).max(50).optional(),
+    phoneNumber: z.string().min(10).max(15).optional(),
+    location: z
+      .object({
+        address: z.string().optional(),
+        city: z.string().optional(),
+        state: z.string().optional(),
+        country: z.string().optional(),
+        zip: z.string().optional(),
+        coordinates: z.tuple([z.number(), z.number()]).optional(),
+      })
+      .optional(),
+  })
+  .strict();
