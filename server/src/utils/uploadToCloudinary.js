0fbb2c69e2456d448f5aec9a9ebcdc8184ca1965
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-07-18 23:19:34 +0530

    update uploadToCloudinary

--- server/src/utils/uploadToCloudinary.js
+++ server/src/utils/uploadToCloudinary.js
@@ -1,8 +1,30 @@
 import cloudinary from "./cloudinary";
 
 export const uploadToCloudinary = async (filePath, folder) => {
+  const transformationMap = {
+    profiles: [
+      {
+        width: 300,
+        height: 300,
+        crop: "thumb",
+        gravity: "face",
+        radius: "max",
+      },
+    ],
+    pets: [
+      {
+        width: 800,
+        height: 800,
+        crop: "fill",
+        gravity: "auto",
+      },
+    ],
+  };
+
+  const transformation = transformationMap[folder] || [];
+
   return await cloudinary.uploader.upload(filePath, {
     folder,
-    transformation: [{ width: 500, height: 500, crop: "limit" }],
+    transformation,
   });
 };
