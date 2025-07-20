51b5a6e494403c410c6e93329abaf3406c01989d
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-07-20 14:27:56 +0530

    wrap context in application with toast feature

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
-    <Toaster position="top-middle" />
+    <Toaster position="top-center" />
     <StrictMode>
       <RouterProvider router={appRouter} />
     </StrictMode>
