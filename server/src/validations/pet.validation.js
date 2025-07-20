5d248cf56dec4f8ec4e8ad366640d43887622ed0
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-07-20 15:14:02 +0530

    update validation by prasing

--- server/src/validations/pet.validation.js
+++ server/src/validations/pet.validation.js
@@ -4,20 +4,21 @@ export const addPetSchema = z.object({
   name: z.string().trim().min(3).max(50),
   species: z.string().trim(),
   breed: z.string().trim().default("Unknown"),
-  age: z.number().min(0),
+  age: z.coerce.number().min(0), // <-- coercion added
   gender: z.enum(["male", "female"]),
   description: z
     .string()
     .trim()
     .max(500, "Description can be up to 500 characters")
     .optional(),
-  vaccinated: z.boolean().optional().default(false),
-  neutered: z.boolean().optional().default(false),
-  adopted: z.boolean().optional().default(false),
+  vaccinated: z.coerce.boolean().optional().default(false), // <-- coercion
+  neutered: z.coerce.boolean().optional().default(false), // <-- coercion
+  adopted: z.coerce.boolean().optional().default(false),
   adoptionDate: z.coerce.date().optional(),
   location: z.string().trim(),
   images: z.array(z.string().url("Each image must be a valid URL")).optional(),
   owner: z
     .string()
-    .regex(/^[0-9a-fA-F]{24}$/, "Owner ID must be a valid MongoDB ObjectId"),
+    .regex(/^[0-9a-fA-F]{24}$/, "Owner ID must be a valid MongoDB ObjectId")
+    .optional(),
 });
