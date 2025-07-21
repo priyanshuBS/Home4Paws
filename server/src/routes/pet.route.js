953b2ad7bdc758af733120f547011a2b743557dc
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-07-21 23:53:04 +0530

    add routes for getAllPets

--- server/src/routes/pet.route.js
+++ server/src/routes/pet.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addPet } from "../controllers/pet.controller.js";
+import { addPet, getAllPets } from "../controllers/pet.controller.js";
 import upload from "../middlewares/multer.middleware.js";
 
 const router = express.Router();
 
 router.route("/add-pet").post(verifyJWT, upload.array("images", 5), addPet);
+router.route("/petsData").get(verifyJWT, getAllPets);
 
 export default router;
