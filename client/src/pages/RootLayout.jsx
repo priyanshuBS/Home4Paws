504e8bf8adb58c9f4cc22626b0e0b7c102622d92
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-07-27 15:50:18 +0530

    update app route

--- client/src/pages/RootLayout.jsx
+++ client/src/pages/RootLayout.jsx
@@ -1,12 +1,15 @@
 import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
+import { AuthInitializer } from "../components/AuthInitializer";
 
 const RootLayout = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <AuthInitializer>
+        <Outlet />
+      </AuthInitializer>
       <Footer />
     </>
   );
