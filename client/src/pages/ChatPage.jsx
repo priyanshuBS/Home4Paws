75f064173546c028735a31b3f72e270613078f96
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-08-12 20:10:21 +0530

    update UI

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -6,7 +6,7 @@ const ChatPage = () => {
 
   return (
     <div className="min-h-screen bg-gray-50 flex justify-center items-center p-2 sm:p-4">
-      <div className="w-full max-w-4xl h-[80vh]">
+      <div className="w-full max-w-4xl h-[90vh]">
         <ChatWindow chatRoomId={chatRoomId} />
       </div>
     </div>
