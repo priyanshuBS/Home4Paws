77551cde5ca63a27224a2cda87d5ed7b22893fbe
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-08-13 23:37:37 +0530

    fix app.js & package.json

--- server/src/app.js
+++ server/src/app.js
@@ -16,13 +16,11 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
-// import routes
 import userRouter from "./routes/user.routes.js";
 import petRouter from "./routes/pet.routes.js";
 import adoptionRouter from "./routes/adoption.routes.js";
 import chatRouter from "./routes/chat.routes.js";
 
-// routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/pets", petRouter);
 app.use("/api/v1/adoption", adoptionRouter);
