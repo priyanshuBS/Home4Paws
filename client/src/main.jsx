9c461e436cb5d52ed55812e4d0c1639240fa0c1f
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-08-13 21:01:45 +0530

    make zoom to 150%

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
@@ -7,13 +7,29 @@ import { AuthProvider } from "./auth/AuthProvider";
 import { Toaster } from "react-hot-toast";
 import { SocketProvider } from "./context/SocketProvider.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+function AppWrapper() {
+  useEffect(() => {
+    if ("zoom" in document.body.style) {
+      document.body.style.zoom = "150%";
+    } else {
+      const root = document.getElementById("root");
+      root.style.transform = "scale(1.5)";
+      root.style.transformOrigin = "0 0";
+    }
+  }, []);
+
+  return (
     <AuthProvider>
       <SocketProvider>
         <Toaster position="top-center" />
         <RouterProvider router={appRouter} />
       </SocketProvider>
     </AuthProvider>
+  );
+}
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <AppWrapper />
   </StrictMode>
 );
