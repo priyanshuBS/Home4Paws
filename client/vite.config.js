71f29c13155b04ddbaf437811db60876d1e35acc
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-08-14 14:51:01 +0530

    update vite.config.js for production

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
+    base: "/",
     plugins: [react(), tailwindcss()],
     server: {
       proxy: {
@@ -16,6 +16,10 @@ export default defineConfig(({ mode }) => {
           secure: false,
         },
       },
+    },
+    build: {
+      outDir: "dist",
+      emptyOutDir: true,
     },
   };
 });
