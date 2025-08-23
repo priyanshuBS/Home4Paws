4eeeef2d528ca18c021f4c4c5152a7e64e992805
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-08-24 00:49:25 +0530

    fix ui

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -27,11 +27,11 @@ const LandingPage = () => {
     <div className="bg-gray-100 px-2 md:px-8">
       {/* hero section */}
       <section className="md:overflow-hidden md:flex md:justify-between items-center md:relative">
-        <div className="container py-20 md:py-0 w-full md:w-6/12 flex flex-col items-center justify-center relative top-8 md:top-14 z-10 text-center">
-          <h1 className="text-5xl md:text-6xl font-extrabold text-gray-800 mb-12 leading-tight drop-shadow-sm md:absolute md:-top-44">
+        <div className="px-2 container py-24 md:py-0 w-full md:w-6/12 flex flex-col items-center justify-center relative top-8 md:top-14 z-10 text-center">
+          <h1 className="text-4xl md:text-6xl font-extrabold text-gray-800 mb-12 leading-tight drop-shadow-sm md:absolute md:-top-44">
             Adopt Your <span className="text-pink-600">Furry Friend</span>
           </h1>
-          <p className="text-lg text-gray-600 mb-12 max-w-2xl">
+          <p className="text-[1rem] text-gray-600 mb-12 max-w-2xl">
             Open your heart and home to a pet waiting to shower you with
             unconditional love. Every adoption changes a life — including yours.
           </p>
